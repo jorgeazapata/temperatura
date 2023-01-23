f4e67aeedc7fde9f60f21e8c6fb519a1809ea1e2
jorgeazapata <jorgeandresz8910@hotmail.com>
2023-01-22 19:19:31 -0500

    first commit

---
+++ script.php
@@ -0,0 +1,9 @@
+<?php
+	//var_dump($_GET);
+	echo('FECHA:   ');
+	echo $_POST['fecha']."<br>";
+	echo('HORA:  ');
+	echo $_POST['hora']."<br>";
+	echo('TEMPERATURA:  ');
+	echo $_POST['temperatura']
+?>
